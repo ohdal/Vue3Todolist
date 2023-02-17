1ac88ac70de588fb4203caedff50dddf2bc9427c
윤현정 <yoonhj97@naver.com>
2023-02-17 16:01:10 +0900

    [ADD] TodoList 컴포넌트 추가

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '/@': path.resolve(__dirname, './src'),
-      '/@ccomponents': path.resolve(__dirname, './src/components'),
+      '/@components': path.resolve(__dirname, './src/components'),
       '/@app_modules': path.resolve(__dirname, './src/modules'),
       '/@store': path.resolve(__dirname, './src/store'),
     }
